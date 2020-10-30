664728f5f4c5fa072b96bda041622e2d2ec46651
mishafominih <mishagjxn@mail.ru>
2020-10-30 23:40:33 +0500

    разработка класса входных данных

---
+++ Energy/Energy/Tick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Energy
+{
+    public class Tick
+    {
+        public List<Device> Devices;
+        public double Usage;
+
+        public Tick(List<Device> d, double u)
+        {
+            Devices = d;
+            Usage = u;
+        }
+    }
+}
