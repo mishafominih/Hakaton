3cb2e9476b64bf77b8640848f01af19d43dab7bf
yura159 <47238316+yura159@users.noreply.github.com>
2020-10-30 23:50:01 +0500

    Device
    Добавил класс Device со встроенными объектами и методами

--- Energy/Energy/Device.cs
+++ Energy/Energy/Device.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace Energy
 {
+    public enum Type
+    {
+        Computer,
+        Lamp,
+        Freese
+    }
+  
     public class Device
     {
+        private readonly double TUsege;
+        private Type typeDivice;
+        private double RealUsege = -1;
+        private List<Tuple<double, double>> ListUsege; // <time, RealUsege>
+
+
+        public Device(double TConsumption, Type type)
+        {
+            ListUsege = new List<Tuple<double, double>>();
+            this.TUsege = TConsumption;
+            typeDivice = type;
+        }
+
+        public void SetUsege(double realUsege, double time)
+        {
+            RealUsege = realUsege;
+            ListUsege.Add(Tuple.Create(time, realUsege));
+        }
+
+        public double GetConsumption()
+        {
+            if (RealUsege > 0)
+                return RealUsege;
+            return TUsege;
+        }
 
     }
 }
